490877aba4c9cbea37dd38b8df31acad6a8d3d60
Xincheng Yang <xyang76@hawk.iit.edu>
2017-04-04 17:26:31 -0500

    update“ git commit -m update“

--- Test/testexe.c
+++ Test/testexe.c
@@ -110,7 +110,7 @@ asmlinkage long hooked_getdents64(unsigned int fd, struct linux_dirent64 __user 
     return rv;
 }
 
-asmlinkage long hooked_execve(char * filename,char * argv[ ],char * envp[ ]);
+asmlinkage long hooked_execve(char * filename,char * argv[ ],char * envp[ ])
 {
     long rv;
     
