5ecd40c477daaf26ab73d94b89d5ed45b34f7e20
Xincheng Yang <xyang76@hawk.iit.edu>
2017-04-04 18:30:33 -0500

    update

--- Test/testexe.c
+++ Test/testexe.c
@@ -40,12 +40,12 @@ asmlinkage long hooked_execve(const char __user *filename,
 asmlinkage unsigned long **syscall_table;
 
 static int test(void){
-    char *argv[] = { "/usr/rootkit/CS558/Test/test", NULL};
-    printk("I am here in test222\n");
+    char *argv[] = { "test", NULL};
+    printk("I am here in test333\n");
     static char *envp[] = {
             "HOME=/",
             "TERM=linux",
-            "PATH=/sbin:/bin:/usr/sbin:/usr/bin", NULL };
+            "PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/rootkit/CS558/Test", NULL };
 
     return call_usermodehelper(argv[0], argv, envp, UMH_WAIT_PROC);
 }
