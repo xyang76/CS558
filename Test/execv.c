e09364ef3af589a8992cea7d6187dc0b24a845d6
Xincheng Yang <xyang76@hawk.iit.edu>
2017-04-10 12:00:11 -0500

    update

--- Test/execv.c
+++ Test/execv.c
@@ -6,7 +6,7 @@
 
 int main(int argc, char **argv)
 {
-    printmsg("Hello");
+    char *msg = "hello";
     char *av[ ]={"/bin/touch", strcat("/usr/rootkit/CS558/Test/", msg), NULL};   
     char *ep[ ]={"PATH=/sbin:/bin:/usr/sbin:/usr/bin:", NULL};   
     execve(av[0], av, ep);  
