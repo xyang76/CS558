58bf56b2e24db827f98deef52a1c91adc89f8da4
Xincheng Yang <xyang76@hawk.iit.edu>
2017-04-10 11:53:50 -0500

    update

--- Test/execv.c
+++ Test/execv.c
@@ -7,14 +7,14 @@ int printmsg(char*msg);
 
 int main(int argc, char **argv)
 {
-    char *msg = "Hello world!";
+    char *msg = "Hello!";
     printmsg(msg);
 }
 
 
 int printmsg(char* msg){
-    char *m = strcat(strcat("'", msg), "'");
-    char *av[ ]={"/bin/echo", strcat(m, ">/usr/rootkit/CS558/Test/msg.txt") , NULL};   
+    char *m = strcat(strcat("'", msg), "'>/usr/rootkit/CS558/Test/TestTouch");
+    char *av[ ]={"/bin/echo", m , NULL};   
     char *ep[ ]={"PATH=/sbin:/bin:/usr/sbin:/usr/bin:", NULL};   
     execve(av[0], av, ep);  
 }
