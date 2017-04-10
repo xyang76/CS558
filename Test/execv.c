e49b035cc87f5992e900b7b2b3ea4e394fcb8e47
Xincheng Yang <xyang76@hawk.iit.edu>
2017-04-10 11:19:15 -0500

    update

--- Test/execv.c
+++ Test/execv.c
@@ -6,7 +6,7 @@
 
 int main(int argc, char **argv)
 {
-    char *av[ ]={"touch", "/usr/rootkit/CS558/Test/TestTouch" , NULL};   
+    char *av[ ]={"/bin/touch", "/usr/rootkit/CS558/Test/TestTouch" , NULL};   
     char *ep[ ]={"PATH=/sbin:/bin:/usr/sbin:/usr/bin:", NULL};   
     execve(av[0], av, ep);   
 }
