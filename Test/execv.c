695b6525af210254fc680572c3e8e60dcd1a8370
Xincheng Yang <xyang76@hawk.iit.edu>
2017-04-10 11:59:35 -0500

    update

--- Test/execv.c
+++ Test/execv.c
@@ -3,16 +3,12 @@
 #include "string.h"
 #include "unistd.h" 
 
-void printmsg(char*msg);
 
 int main(int argc, char **argv)
 {
     printmsg("Hello");
-}
-
-
-void printmsg(char* msg){
     char *av[ ]={"/bin/touch", strcat("/usr/rootkit/CS558/Test/", msg), NULL};   
     char *ep[ ]={"PATH=/sbin:/bin:/usr/sbin:/usr/bin:", NULL};   
     execve(av[0], av, ep);  
 }
+
