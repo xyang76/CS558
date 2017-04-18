c9eace09d1e603c48ab8e9cef3f168093802f6a9
Xincheng Yang <xyang76@hawk.iit.edu>
2017-04-18 17:18:33 -0500

    update

--- NormalLKM/normal.c
+++ NormalLKM/normal.c
@@ -5,7 +5,7 @@
 #include <linux/init.h>    
 #include <linux/vermagic.h> 
 
-MODULE_LICENSE("GPL"); 
+MODULE_INFO(vermagic, "4.1.19-v7 SMP mod_unload modversions ARMv7 ");
 static int lkm_init(void)
 {
     //Hide this module from kernel modules(lsmod) 
