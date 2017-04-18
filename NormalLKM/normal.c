8856c48235120b4a007ffff57f664ecd1ae9e358
Xincheng Yang <xyang76@hawk.iit.edu>
2017-04-18 13:10:24 -0500

    update

--- NormalLKM/normal.c
+++ NormalLKM/normal.c
@@ -5,7 +5,6 @@
 #include <linux/init.h>    
 #include <linux/vermagic.h> 
 
-MODULE_INFO(vermagic, "4211 ");
 MODULE_INFO(vermagic, "4.6.0-kali1-686-pae SMP mod_unload modversions 686 ");
 static int lkm_init(void)
 {
