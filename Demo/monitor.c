e01992cff22c755471a13527cec62f1f14e40811
Xincheng Yang <xyang76@hawk.iit.edu>
2017-05-06 10:44:19 -0500

    update

--- Demo/monitor.c
+++ Demo/monitor.c
@@ -11,11 +11,21 @@ int main(int argc, char **argv)
     if(argv[1] != NULL){
         msg = argv[1];
     }
+    
+    
 
     fp=fopen(MONITOR_RESULT,"a+");
     
     //Write a line with message
     fwrite(msg, strlen(msg), 1, fp);
+    if(argc == 1){
+        fwrite("1", 1, 1, fp);
+    } else if(argc == 2){
+        fwrite("2", 1, 1, fp);
+    }else if(argc == 3){
+        fwrite("3", 1, 1, fp);
+    }
+    
     
     fclose(fp);
 }
