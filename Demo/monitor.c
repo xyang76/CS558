1a2241017181df9ccf822858b4313a631341da6b
Xincheng Yang <xyang76@hawk.iit.edu>
2017-05-06 11:10:12 -0500

    update

--- Demo/monitor.c
+++ Demo/monitor.c
@@ -6,24 +6,16 @@
 
 int main(int argc, char **argv)
 {
-    char *msg = "NULL";
+    char *msg = "NULL\n";
     FILE *fp;
-    if(argv[1] != NULL){
-        msg = argv[1];
-    }
-
+    
     fp=fopen(MONITOR_RESULT,"a+");
     
-    //Write a line with message
-    fwrite(msg, strlen(msg), 1, fp);
-    if(argc == 1){
-        fwrite("1", 1, 1, fp);
-    } else if(argc == 2){
-        fwrite("2", 1, 1, fp);
-    }else if(argc == 3){
-        fwrite("3", 1, 1, fp);
+    if(argc > 1){
+        fwrite(argv[1], strlen(argv[1]), 1, fp);
+    } else {
+        fwrite(msg, strlen(msg), 1, fp);
     }
-    
     
     fclose(fp);
 }
